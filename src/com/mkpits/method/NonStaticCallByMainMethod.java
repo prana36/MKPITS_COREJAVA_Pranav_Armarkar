174616f5a594fe42ff0fd1c35475c6907973cc7a
prana36 <149050054+prana36@users.noreply.github.com>
2023-11-30 11:52:15 +0530

    Update

--- src/com/mkpits/method/NonStaticCallByMainMethod.java
+++ src/com/mkpits/method/NonStaticCallByMainMethod.java
@@ -9,7 +9,7 @@ public class NonStaticCallByMainMethod {
 		
 		System.out.println("MY First Name is : -" +nsmwrt.getFirstName());
 		System.out.println("My last Name is :- " +nsmwrt.getLastName());
-		System.out.println("My Email is :- " +nsmwrt.getEmail());
+		System.out.println("My Vailed Email is :-"+nsmwrt.getEmail());
 		System.out.println("My Password is :-  " +nsmwrt.getPassword());
 		System.out.println("Mobile No. :- " +nsmwrt.getmobile());
 		System.out.println("Gender :- " +nsmwrt.getgender());
