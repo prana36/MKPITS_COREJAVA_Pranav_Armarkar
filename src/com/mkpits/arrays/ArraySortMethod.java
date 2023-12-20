bfb1579f04fe4d48db912a934fe3ee712cdfa55e
prana36 <149050054+prana36@users.noreply.github.com>
2023-12-20 20:48:36 +0530

    Math and string added

--- src/com/mkpits/arrays/ArraySortMethod.java
+++ src/com/mkpits/arrays/ArraySortMethod.java
@@ -4,10 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class ArraySortMethod {
+	
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		
+		ArraySortMethod asm = new ArraySortMethod();
+		asm.sorting();
+		
+		
+	}
+
+	
+
+	private void sorting() throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter range :- ");
 		int r=Integer.parseInt(br.readLine());
@@ -27,12 +41,27 @@ public class ArraySortMethod {
 		
 		
 		System.out.println("After sorting \n");
-        
+		
+		
+		
+		System.out.println("Acending Order ");
+        // these is using for sorting to acending order
 		Arrays.sort(num);
 		
 		for (String number:num ) {
 			System.out.println( number);
 		}
+		
+	
+		
+		System.out.println("Decending order \n");
+		Arrays.sort(num, Collections.reverseOrder());
+		for (String reverse : num) {
+			System.out.println(reverse);
+			
+		}
+		
 	}
+	
 
 }
